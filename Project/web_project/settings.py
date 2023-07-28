ed4f7c6eba7790fb8a2bc579a889fa594d75c75a
Arm117 <43988537+Arm117@users.noreply.github.com>
2023-07-28 19:00:00 +0700

    updated web_project files
    added project.html, updated settings.py, url.py, and views.py

--- Project/web_project/settings.py
+++ Project/web_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    
+    'enrollsys',
     #'rest_framework',
 
 ]
--- Project/web_project/urls.py
+++ Project/web_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from enrollsys import views
 
 urlpatterns = [
+
+    path("", include("enrollsys.urls")),
     path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
+    #path("hello/<name>", views.hello_there, name="hello_there"),
+
 ]
